866289e235c2d0752b064d9201b8ef772126e9e1
SaiNeeraj2503 <neerajchandragiri888@gmail.com>
2023-09-18 11:33:57 -0500

    code push

--- Assignments/P01/cmd_pkg_use/cmd_pkg/cmdLs.py
+++ Assignments/P01/cmd_pkg_use/cmd_pkg/cmdLs.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python
 import subprocess
+import platform
 
 def ls(**kwargs):
-    """This is my manpage entry for the pwd command
-    """
-    result = subprocess.run(['ls','-lah'], stdout=subprocess.PIPE)
-    
-    return result.stdout.decode('utf-8')
+    if platform.system() == "Windows":
+        command = ["powershell", "Get-ChildItem"]
+        if "params" in kwargs:
+            params = kwargs["params"]
+            command.extend(params)
+        result = subprocess.run(command, stdout=subprocess.PIPE)
+        output = result.stdout.decode('utf-8')
+        lines = output.split('\n')
+        file_name=""
+        for line in lines[7:]:  # as the output file had 7 empty lines used this to skip those first 7 lines
+            parts = line.split() 
+            if len(parts) > 1:  # Check if there are more than one part (date details stuff,name)
+                file_name+=parts[-1]+ "  " # Print the last part, which is the file name
+        print(file_name)
+    else:
+        import os
+        if "params" in kwargs:
+            directory = kwargs["params"][0]
+        else:
+            directory = "."  # List the current directory by default
+
+        try:
+            files = os.listdir(directory)
+            for file in files:
+                print(file)  # Print the file names
+        except FileNotFoundError:
+            print(f"Directory not found: {directory}")
+        except PermissionError:
+            print(f"Permission denied: {directory}")
--- Assignments/P01/cmd_pkg_use/cmd_pkg/cmdPwd.py
+++ Assignments/P01/cmd_pkg_use/cmd_pkg/cmdPwd.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 import subprocess
+import platform
+import os
 
 def pwd(**kwargs):
     """This is my manpage entry for the pwd command
     """
-    result = subprocess.run(['pwd'], stdout=subprocess.PIPE)
+    if platform.system() == "Windows":
+        # this runs if the os is windows
+        result = subprocess.run(["cd"], stdout=subprocess.PIPE, shell=True)
+        output = result.stdout.decode('utf-8')
+        print(output.strip())
+    else:
+        # if the os is unix based, the code goes in this condition
+        current_directory = os.getcwd()
+        print(current_directory)
     
-    return result.stdout.decode('utf-8')
 
 
-if __name__=='__main__':
-    print(pwd())
+# if __name__=='__main__':
+#     print(pwd())
